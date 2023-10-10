e65f38ed392c32add8468c9a5ef1914e8a4319f1
avidNewb <59672137+avidNewb@users.noreply.github.com>
2023-10-09 20:09:08 -0500

    Update axilla.js

--- functions/axilla/axilla.js
+++ functions/axilla/axilla.js
@@ -221,12 +221,12 @@ exports.handler = async (event) => {
     try {
       const database = (await clientPromise).db(process.env.MONGODB_DATABASE);
       const collection = database.collection(process.env.MONGODB_COLLECTION);
-      const results = await collection.find({}).limit(10).toArray();
+      const results2 = await collection.find({}).limit(10).toArray();
     } catch (error) {
       return { statusCode: 500, body: error.toString() }
     }
     const formData = new FormData();
-    formData.append('config', JSON.stringify(results));
+    formData.append('config', JSON.stringify(results2));
 
     // send the form data
     fetch('https://example.com/api/v1/users', {
