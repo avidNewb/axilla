93f1514422c8813b868153f75e67c3940559bf9e
avidNewb <59672137+avidNewb@users.noreply.github.com>
2023-10-09 21:59:48 -0500

    Update axilla.js

--- functions/axilla/axilla.js
+++ functions/axilla/axilla.js
@@ -220,13 +220,13 @@ exports.handler = async (event) => {
   else{
     
     const formData = new FormData();
-   // var jsonObj;
+    var jsonObj;
     try {
       const database = (await clientPromise).db(process.env.MONGODB_DATABASE);
       const collection = database.collection(process.env.MONGODB_COLLECTION);
       const results = await collection.find({}).limit(10).toArray();
-      //jsonObj = JSON.parse(results);
-      formData.append('config', JSON.stringify(results));
+      jsonObj = JSON.parse(results);
+      //formData.append('config', JSON.stringify(results));
     } catch (error) {
       return { statusCode: 500, body: error.toString() }
     }
@@ -234,27 +234,33 @@ exports.handler = async (event) => {
       // raw image
       case OUTPUTS.IMAGE:
          // formData.append(format,imageBase64)
+          jsonObj[`${format}`] = JSON.stringify(results);
           return{
             statusCode: 200,
             headers: {'content-type':'multipart/form-data'},
-            body: formData
+            //body: formData
+            body: JSON.stringify(jsonObj)
           }
 
       // base64 image text
       case OUTPUTS.BASE64:
        // formData.append("base64",imageBase64);
+          jsonObj[`base64`] = JSON.stringify(results);
         return{
           statusCode: 200,
           headers: {'content-type':'multipart/form-data'},
-          body: formData
+          //body: formData
+          body: JSON.stringify(jsonObj)
         }
 
       default:
        // formData.append("webp",imageBase64)
+          jsonObj[`webp`] = JSON.stringify(results);
         return{
           statusCode: 200,
           headers: {'content-type':'multipart/form-data'},
-          body: formData
+          //body: formData
+          body: JSON.stringify(jsonObj)
         }
     }
   }
